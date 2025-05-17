f38b4846026bda9e6fa4e08bede1eca7e070b0d8
Vincent <courtois.vincent@outlook.com>
2025-05-17 14:22:25 +0200

    Finish the menu with the existing pages

--- components/Navbar/Desktop.vue
+++ components/Navbar/Desktop.vue
@@ -1,6 +1,22 @@
 <template>
-  <v-app-bar color="grey darken-2">
-    {{ props.authenticated }}
+  <v-app-bar color="deep-purple darken-2" v-if="authenticated">
+    <template v-slot:prepend>
+      <v-app-bar-title class="ml-2 mr-1">{{ $t('common.brand') }}</v-app-bar-title>
+      <v-btn icon to="/" aria-label="home link" class="ml-1">
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+      <v-btn variant="text" to="/synthesizers" class="ml-2">{{ $t('menu.synthesizers') }}</v-btn>
+    </template>
+  </v-app-bar>
+  <v-app-bar color="deep-purple darken-2" v-else>
+    <template v-slot:prepend>
+      <v-app-bar-title class="mr-2">{{ $t('common.brand') }}</v-app-bar-title>
+      <v-btn icon to="/" aria-label="home link"><v-icon>mdi-home</v-icon></v-btn>
+    </template>
+    <template v-slot:append>
+      <v-btn to="/login" aria-label="login link">{{ $t('common.verbs.login') }}</v-btn>
+      <language-switch />
+    </template>
   </v-app-bar>
 </template>
 
--- components/Navbar/Mobile.vue
+++ components/Navbar/Mobile.vue
@@ -1,6 +1,27 @@
 <template>
-  <v-app-bar color="deep-purple darken-2">
-    {{ props.authenticated }}
+  <v-btn icon to="/" aria-label="home link">
+    <v-icon>mdi-home</v-icon>
+  </v-btn>
+  <v-app-bar color="deep-purple darken-2" v-if="authenticated">
+    <v-btn icon to="/synthesizers" class="ml-2" aria-label="synthesizers list">
+      <v-icon>mdi-piano</v-icon>
+    </v-btn>
+  </v-app-bar>
+  <v-app-bar v-else>
+    <template v-slot:append>
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn icon v-bind="props">
+            <v-icon>mdi-menu</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item to="/login">
+            <v-list-item-title>{{ $t('common.verbs.login') }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </template>
   </v-app-bar>
 </template>
 
