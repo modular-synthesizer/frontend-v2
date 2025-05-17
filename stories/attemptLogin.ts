cdb8779b4d5c68b085d72f718205f729ae9548c3
Vincent <courtois.vincent@outlook.com>
2025-05-17 10:56:06 +0200

    Add a bit more unit tests to check if the underlying async layer is correctly called

--- stories/attemptLogin.ts
+++ stories/attemptLogin.ts
@@ -6,15 +6,17 @@ type Snacker = {
   asyncError: (failure: AsyncFailure) => void
 }
 
+type Navigator = typeof navigateTo;
+
 /**
  * this user story allows the user to attempt to login on the application. it creates the function
  * that will be called from the asynchronous procedure around the presentational login form. This
  * function will only forward the arguments to the API on the correct route.
  */
-export function attemptLogin(loginRequester: LoginRequester, snacker: Snacker) {
+export function attemptLogin(loginRequester: LoginRequester, snacker: Snacker, navigator: Navigator) {
   return async (username: string, password: string) => {
     const response = await loginRequester(username, password);
     // Display the correct message in the snackbar queue depending on the state of the response.
-    return response.ok ? navigateTo('/') : snacker.asyncError(response)
+    return response.ok ? navigator('/') : snacker.asyncError(response)
   }
 }
--- tests/stories/attemptLogin.test.ts
+++ tests/stories/attemptLogin.test.ts
@@ -1,21 +1,39 @@
 import { expect, test, vi } from "vitest"
 import { attemptLogin } from "../../stories/attemptLogin"
 
-const fakeRequesterError: LoginRequester = (username: string, password: string) => {
-  return { ok: false, key: 'username', message: 'unknown' }
+const fakes = {
+  requestError: (username: string, password: string) => {
+    return { ok: false, key: 'username', message: 'unknown' }
+  },
+  requestSuccess: (username: string, password: string) => {
+    return { ok: true, data: { } }
+  },
+  navigator: (path: string) => { },
 }
-const fakeRequesterSuccess: LoginRequester = (username: string, password: string) => {
-  return { ok: true, data: { } }
-}
+
 const fakeSnacker: Snacker = {
   success(test: string) { },
   error(test: string) { },
   asyncError(failure: AsyncFailure) { },
 }
 
+test("Calls the asynchronous function with the correct parameters", async () => {
+  const asyncSpy = vi.spyOn(fakes, "requestError")
+  const story = attemptLogin(fakes.requestError, fakeSnacker, fakes.navigator)
+  await story("anyUsername", "anyPassword")
+  expect(asyncSpy).toHaveBeenCalledWith("anyUsername", "anyPassword")
+})
+
 test("Fails if the underlying asynchronous call is not successful", async () => {
-  const story = attemptLogin(fakeRequesterError, fakeSnacker)
   const errorSpy = vi.spyOn(fakeSnacker, "asyncError")
+  const story = attemptLogin(fakes.requestError, fakeSnacker, fakes.navigator)
   await story("wrongUsername", "wrongPassword")
-  expect(errorSpy).toHaveBeenCalled()
+  expect(errorSpy).toHaveBeenCalledOnce()
+})
+
+test("Redirects if the underlying asynchronous call is successful", async () => {
+  const navigatorSpy = vi.spyOn(fakes, "navigator")
+  const story = attemptLogin(fakes.requestSuccess, fakeSnacker, fakes.navigator)
+  await story("correctUsername", "correctPassword")
+  expect(navigatorSpy).toHaveBeenCalledOnce()
 })
