5b1f2fd46010d62833c18944943cdb9ad7332355
Vincent COURTOIS <vincent.courtois@ecov.fr>
2025-05-20 14:53:21 +0200

    Decompose the feature to remove synthesizer into two separate features

--- stories/synthesizers/removeSynthesizer.ts
+++ stories/synthesizers/removeSynthesizer.ts
@@ -1,12 +1,24 @@
 type Requester = (id: string) => AsyncResult<void>
 
+/**
+ * This story is used in several cases :
+ * 1. A user removes a synthesizer on the API successfully and it's deleted from the interface.
+ * 2. A user receives a message from the SSE socket to remove a synthesizer he's been banned from.
+ * 
+ * @param id The UUID of the synthesizer to delete from the list
+ * @param list The list of synthesizer from which the syntheiszer will be deleted.
+ */
+export function deleteSynthesizerFromlist(id: string, list: Synthesizer[]) {
+  const index: number = list.findIndex((s: Synthesizer) => s.id === id)
+  if (index >= 0) list.splice(index, 1)
+}
+
 export function removeSynthesizer(requester: Requester, snacker: Snacker, list: Synthesizer[]) {
   return async (id: string) => {
     const response: AsyncResult<void> = await requester(id);
     if (response.ok) {
       snacker.success('synthesizers.deletion.success');
-      const index: number = list.findIndex((s: Synthesizer) => s.id === id)
-      if (index >= 0) list.splice(index, 1)
+      deleteSynthesizerFromlist(id, list)
     }
     else {
       snacker.error('synthesizers.deletion.error');
