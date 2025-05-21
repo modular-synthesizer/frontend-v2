91e8955eba53a8e6d4a49b774029cb60310596c3
Vincent <courtois.vincent@outlook.com>
2025-05-21 23:09:13 +0200

    Add the draggable scene to the components to display a synthesizer

---
+++ components/Draggable/Scene.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="scene-wrapper">
+    <slot name="layers" />
+  </div>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  width: string
+  height: string
+  color: string
+}
+const { height = '100vh', width = '100vw', color = 'silver' } = defineProps<Props>()
+</script>
+
+<style scoped>
+.scene-wrapper {
+  height: v-bind(height);
+  width: v-bind(width);
+  background-color: v-bind(color);
+}
+</style>
--- pages/synthesizers/[id].vue
+++ pages/synthesizers/[id].vue
@@ -1,9 +1,8 @@
 <template>
-  <pre v-if="synthesizer">
-    {{ synthesizer }}
-  </pre>
+  <draggable-scene v-if="synthesizer" />
 </template>
 
 <script setup lang="ts">
+definePageMeta({ layout: false })
 const synthesizer = ref(await features.synthesizers.fetch(`${useRoute().params.id}`))
 </script>
