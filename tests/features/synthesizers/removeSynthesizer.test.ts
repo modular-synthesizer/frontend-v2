566bb4de52afec8cc11d88ec8d44744dc736d843
Vincent COURTOIS <vincent.courtois@ecov.fr>
2025-05-28 17:37:48 +0200

    Fix some more typing issues in the removeSynthesizer tests

--- tests/features/synthesizers/removeSynthesizer.test.ts
+++ tests/features/synthesizers/removeSynthesizer.test.ts
@@ -1,18 +1,19 @@
 import { afterEach, expect, test, vi } from 'vitest'
 import { fakeErrorApi, fakeSnacker, fakeSuccessApi } from '../../fakes'
 import { removeSynthesizer } from "../../../core/features/synthesizers/removeSynthesizer"
+import { SynthesizerFactory } from '~/tests/factories/synthesizers'
 
-const fakeData = () => {
+const fakeData = async () => {
   return [
-    { id: 'firstSynth', voices: 1, name: 'First Synth', members: [ ], modules: [ ], scale: 1.0 },
-    { id: 'secondSynth', voices: 1, name: 'Second Synth', members: [ ], modules: [ ], scale: 1.0 },
+    await SynthesizerFactory.props({ id: () => 'firstSynth', name: () => 'First Synth' }).build(),
+    await SynthesizerFactory.props({ id: () => 'secondSynth', name: () => 'Second Synth' }).build(),
   ]
 }
 
 afterEach(() => vi.restoreAllMocks())
 
 test('Correctly delets the synthesizer from the list', async () => {
-  const data = fakeData()
+  const data = await fakeData()
   await removeSynthesizer(fakeSuccessApi, fakeSnacker)("firstSynth", data)
   expect(data.length).toBe(1)
 })
@@ -20,19 +21,19 @@ test('Correctly delets the synthesizer from the list', async () => {
 test('Displays a success snack if the synthesizer is correctly deleted', async () => {
   const snackerSpy = vi.spyOn(fakeSnacker, "success")
   const story = removeSynthesizer(fakeSuccessApi, fakeSnacker)
-  await story("secondSynth", fakeData())
+  await story("secondSynth", await fakeData())
   expect(snackerSpy).toHaveBeenCalledExactlyOnceWith("synthesizers.deletion.success")
 })
 
 test('Displays an error snack if the synthesizer fails to be deleted', async () => {
   const snackerSpy = vi.spyOn(fakeSnacker, "error")
   const story = removeSynthesizer(fakeErrorApi, fakeSnacker)
-  await story("secondSynth", fakeData())
+  await story("secondSynth", await fakeData())
   expect(snackerSpy).toHaveBeenCalledExactlyOnceWith("synthesizers.deletion.error")
 })
 
 test('Does not delete any synthesizer in the list if the synthesizer is not in it', async () => {
-  const data = fakeData()
+  const data = await fakeData()
   await removeSynthesizer(fakeErrorApi, fakeSnacker)("thirdSynth", data)
   expect(data.length).toBe(2)
 })
