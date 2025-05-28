b6d3c1c48e6c1ee3558e21f2363b50325e435728
Vincent COURTOIS <vincent.courtois@ecov.fr>
2025-05-28 17:05:28 +0200

    Move the constants to a single file

--- core/constants.ts
+++ core/constants.ts
@@ -1,2 +1,18 @@
+// This value is kind of magic. It has been determined after trials and
+// errors as the best value to apply to the wheel scroll to zoom in and
+// out smoothly without it being too fast or too slow.
+export const ZOOM_RATIO = 0.005;
+
+// This value is the maximum value the ratio can be "zoomed out" with
+// It is a magic value obtained after some trials and errors too.
+export const MAX_ZOOM_OUT = 0.5;
+
+// This value is the maximum value the ratio can be "zoomed in" with
+// It is a magic value obtained after some trials and errors too.
+export const MAX_ZOOM_IN = 2;
+
+// The height of a rack in pixels when zooming at scale 1.0
 export const RACK_HEIGHT=400
+
+// The width of an HP (slot) in pixels when zooming at scale 1.0
 export const SLOT_WIDTH=20
