8f8346e10e0212398f1809575bbb9b982b572237
Vincent COURTOIS <vincent.courtois@ecov.fr>
2025-06-25 14:18:49 +0200

    feat(#5): add a first version of the dialog button component

---
+++ components/Tools/creation/Form.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-btn @click="submit">pouet</v-btn>
+</template>
+
+<script setup lang="ts">
+
+const emit = defineEmits<{ submit: [ ] }>()
+
+const submit = () => {
+  if (confirm("on ferme ?")) {
+    emit("submit")
+  }
+}
+
+defineExpose({ submit })
+</script>
