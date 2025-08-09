fe8b3cdc148b5da65ad1d3a9ecf6b2dfad010501
Vincent COURTOIS <vincent.courtois@ecov.fr>
2025-08-09 10:02:10 +0200

    Add a rule to force the presence of a name at a tool's creation

--- components/Tools/creation/Form.vue
+++ components/Tools/creation/Form.vue
@@ -1,11 +1,26 @@
 <template>
-  <v-text-field v-model="tool.name" placeholder="Ce nom sera utilisé comme clef de traduction" label="Nom de l'outil" />
-  <v-slider v-model="tool.slots" :min="2" :max="50" :step="1" thumb-label label="Emplacements" />
-  <categories-list v-model="tool.categoryId" />
+  <v-form ref="form">
+    <v-text-field :rules="rules.name" v-model="tool.name" placeholder="Ce nom sera utilisé comme clef de traduction" label="Nom de l'outil" />
+    <v-slider v-model="tool.slots" :min="2" :max="50" :step="1" thumb-label label="Emplacements" />
+    <categories-list v-model="tool.categoryId" />
+  </v-form>
 </template>
 
 <script setup lang="ts">
 const { tool } = defineProps<{ tool: Tool }>()
+const form = ref()
 
-defineExpose({ tool })
+const { t } = useI18n()
+
+async function validate() {
+  return (await form.value.validate()).valid
+}
+
+defineExpose({ validate, tool })
+
+const rules = {
+  name: [
+    (value: string) => value ?? t("tools.create.name.required")
+  ]
+}
 </script>
