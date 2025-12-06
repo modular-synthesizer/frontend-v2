0ed768506675c0b705a167d6dbcd410f142f30bf
Vincent <courtois.vincent@outlook.com>
2025-12-06 12:37:38 +0100

    Bump SDK dependencies

--- core/functions/modules/parameters.ts
+++ core/functions/modules/parameters.ts
@@ -2,6 +2,11 @@ import type { Parameter } from "~/core/business/synthesizers/Parameter.type";
 import { round } from "../maths";
 
 export function getValue(parameter: Parameter) {
-	const { value = 0, precision = 0 } = parameter;
-	return round(value, precision);
+	try {
+		const { value = 0, precision = 0 } = parameter;
+		return round(value, precision);
+	}
+	catch (_) {
+		return 0
+	}
 }
